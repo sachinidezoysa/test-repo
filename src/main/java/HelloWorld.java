a086962837fce1ab26eae01e1da80cca8ebf0c58
sachinidezoysa <bawanthisachini1995@gmail.com>
2018-07-09 15:50:42 +0530

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public void sayHello(){
+
+        System.out.print("Hello world!!!");
+    }
+}
---
+++ src/main/java/Print.java
@@ -0,0 +1,10 @@
+public class Print {
+
+    public static void main(String[]args){
+
+        HelloWorld helloWorld = new HelloWorld();
+
+        helloWorld.sayHello();
+    }
+
+}
